91bdbbfadc9be969fae6dde007fe25cd1282fe61
rwise <robertwise64@gmail.com>
2020-12-02 10:56:51 -0600

    Merge sorted lists

--- epi_judge_java/epi/SortedListsMerge.java
+++ epi_judge_java/epi/SortedListsMerge.java
@@ -4,11 +4,65 @@ import epi.test_framework.GenericTest;
 public class SortedListsMerge {
   @EpiTest(testDataFile = "sorted_lists_merge.tsv")
   //@include
-  public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> L1,
+  // Solution using dummyHead as suggested by EPI, ridiculously more concise...
+    public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> L1,
                                                       ListNode<Integer> L2) {
-    // TODO - you fill in here.
-    return null;
+    ListNode<Integer> dummyHead = new ListNode(0, null), iter = dummyHead;
+    while (L1 != null && L2 != null) {
+      if (L1.data <= L2.data) {
+        iter.next = L1;
+        L1 = L1.next;
+      }
+      else {
+        iter.next = L2;
+        L2 = L2.next;
+      }
+      iter = iter.next;
+    }
+    if (L1 != null)
+      iter.next = L1;
+    else
+      iter.next = L2;
+    return dummyHead.next;
   }
+
+//  Initial Solution: no dummyHead
+//  public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> L1,
+//                                                      ListNode<Integer> L2) {
+//    ListNode<Integer> head = null, iter = null;
+//    if (L1 == null && L2 == null)
+//      return null;
+//    if (L1 == null)
+//      return L2;
+//    if (L2 == null)
+//      return L1;
+//    if (L1.data <= L2.data) {
+//      head = L1;
+//      iter = L1;
+//      L1 = L1.next;
+//    }
+//    else {
+//      head = L2;
+//      iter = L2;
+//      L2 = L2.next;
+//    }
+//    while (L1 != null && L2 != null) {
+//      if (L1.data <= L2.data) {
+//        iter.next = L1;
+//        L1 = L1.next;
+//      }
+//      else {
+//        iter.next = L2;
+//        L2 = L2.next;
+//      }
+//      iter = iter.next;
+//    }
+//    if (L1 != null)
+//      iter.next = L1;
+//    else
+//      iter.next = L2;
+//    return head;
+//  }
 
   public static void main(String[] args) {
     System.exit(
--- epi_judge_java/epi/StringIntegerInterconversion.java
+++ epi_judge_java/epi/StringIntegerInterconversion.java
@@ -1,49 +1,66 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
+
 public class StringIntegerInterconversion {
 
-  public static String intToString(int x) {
-    StringBuilder sb = new StringBuilder();
-    boolean neg = false;
-    if (x < 0)
-      neg = true;
-    do {
-      sb.append((char) ('0' + Math.abs(x % 10)));
-      x /= 10;
-    } while (x != 0);
-    if (neg)
-      sb.append('-');
-    return sb.reverse().toString();
-  }
-  public static int stringToInt(String s) {
-    int res = 0;
-    boolean neg = false;
-    for (int i = 0; i < s.length(); i++) {
-      res *= 10;
-      if (s.charAt(i) == '-')
-        neg = true;
-      else if (s.charAt(i) != '+')
-        res += s.charAt(i) - '0';
+    public static String intToString(int x) {
+        StringBuilder sb = new StringBuilder();
+        boolean neg = false;
+        if (x < 0)
+            neg = true;
+        do {
+            sb.append((char) ('0' + Math.abs(x % 10)));
+            x /= 10;
+        } while (x != 0);
+        if (neg)
+            sb.append('-');
+        return sb.reverse().toString();
     }
-    return res * ((neg) ? -1 : 1);
-  }
-  @EpiTest(testDataFile = "string_integer_interconversion.tsv")
-  public static void wrapper(int x, String s) throws TestFailure {
-    if (Integer.parseInt(intToString(x)) != x) {
-      throw new TestFailure("Int to string conversion failed");
-    }
-    if (stringToInt(s) != x) {
-      throw new TestFailure("String to int conversion failed");
-    }
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "StringIntegerInterconversion.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    // Trying out the book version since it uses many new functions to me
+    public static int stringToInt(String s) {
+        // Multiply by -1 if negative
+        return (s.charAt(0) == '-' ? -1 : 1) *
+                // Skip negative or positive (if present)
+                s.substring((s.charAt(0) == '-' || s.charAt(0) == '+') ? 1 : 0)
+                        // Get char stream
+                        .chars()
+                        // Perform operation on each value, shifting sum over 1 and adding the new value
+                        .reduce(0, (sum, c) -> sum * 10 + c - '0');
+    }
+
+    //  My initial version
+//  public static int stringToInt(String s) {
+//    int res = 0;
+//    boolean neg = false;
+//    for (int i = 0; i < s.length(); i++) {
+//      res *= 10;
+//      if (s.charAt(i) == '-')
+//        neg = true;
+//      else if (s.charAt(i) != '+')
+//        res += s.charAt(i) - '0';
+//    }
+//    return res * ((neg) ? -1 : 1);
+//  }
+    @EpiTest(testDataFile = "string_integer_interconversion.tsv")
+    public static void wrapper(int x, String s) throws TestFailure {
+        if (Integer.parseInt(intToString(x)) != x) {
+            throw new TestFailure("Int to string conversion failed");
+        }
+        if (stringToInt(s) != x) {
+            throw new TestFailure("String to int conversion failed");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "StringIntegerInterconversion.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
