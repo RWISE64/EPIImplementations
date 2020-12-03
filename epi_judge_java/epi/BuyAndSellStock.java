13aa2e62549d3aee4c5d7ab7feaeba0a0031efba
rwise <robertwise64@gmail.com>
2020-12-03 11:43:09 -0600

    Buy and Sell Stock

--- epi_judge_java/epi/BuyAndSellStock.java
+++ epi_judge_java/epi/BuyAndSellStock.java
@@ -5,9 +5,24 @@ import epi.test_framework.GenericTest;
 import java.util.List;
 public class BuyAndSellStock {
   @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
+  /*
+  This solution utilizes the fact that the buy always needs to take place before the sell
+  Due to this, can just track minimum as you iterate through, and continually compare with next encountered price
+  Time: O(n), where n = prices.size() (just one iteration through prices)
+  Space: O(1) (constant amount of variables, not dependent on size of prices)
+   */
   public static double computeMaxProfit(List<Double> prices) {
-    // TODO - you fill in here.
-    return 0.0;
+    // Tracks to lowest encountered price during a left to right traversal
+    int curMinInd = 0;
+    double curMaxProf = 0;
+    for (int i = 0; i < prices.size(); i++) {
+      // Update current min if necessary
+      if (prices.get(i) < prices.get(curMinInd))
+        curMinInd = i;
+      // Keep maximum encountered profit
+      curMaxProf = Math.max(prices.get(i) - prices.get(curMinInd), curMaxProf);
+    }
+    return curMaxProf;
   }
 
   public static void main(String[] args) {
