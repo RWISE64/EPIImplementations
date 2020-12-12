cfc48546326915c1b63877d614113d08d85c607c
rwise <robertwise64@gmail.com>
2020-12-11 18:17:06 -0600

    K Closest Stars

--- epi_judge_java/epi/KClosestStars.java
+++ epi_judge_java/epi/KClosestStars.java
@@ -1,69 +1,87 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.EpiTestExpectedType;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.function.BiPredicate;
+
 public class KClosestStars {
-  @EpiUserType(ctorParams = {double.class, double.class, double.class})
+    @EpiUserType(ctorParams = {double.class, double.class, double.class})
 
-  public static class Star implements Comparable<Star> {
-    private double x, y, z;
+    public static class Star implements Comparable<Star> {
+        private double x, y, z;
 
-    public Star(double x, double y, double z) {
-      this.x = x;
-      this.y = y;
-      this.z = z;
+        public Star(double x, double y, double z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public double distance() {
+            return Math.sqrt(x * x + y * y + z * z);
+        }
+
+        @Override
+        public int compareTo(Star that) {
+            return Double.compare(this.distance(), that.distance());
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(distance());
+        }
     }
 
-    public double distance() { return Math.sqrt(x * x + y * y + z * z); }
-
-    @Override
-    public int compareTo(Star that) {
-      return Double.compare(this.distance(), that.distance());
+    /*
+    Just used a maxHeap so that the largest value is always removed first, leaving us with the k smallest values
+    Time: O(nlogn), where n = number of elements. This is the case since insertions take logn time with heaps
+    Space: O(k), as heap maxes out at size k (or k + 1 for a moment)
+     */
+    public static List<Star> findClosestKStars(Iterator<Star> stars, int k) {
+        // I know it was implemented with comparable, but I wanted to actually write the comparison myself
+        // Should place the larger distance first
+        PriorityQueue<Star> maxHeap = new PriorityQueue<Star>(k, (s1, s2) -> Double.compare(s2.distance(), s1.distance()));
+        while(stars.hasNext()) {
+            maxHeap.add(stars.next());
+            // Remove top if heap is larger than k. This should remove the furthest away star of the heap
+            if (maxHeap.size() > k)
+                maxHeap.poll();
+        }
+        return new ArrayList(maxHeap);
     }
 
-    @Override
-    public String toString() {
-      return String.valueOf(distance());
+    @EpiTest(testDataFile = "k_closest_stars.tsv")
+    public static List<Star> findClosestKStarsWrapper(List<Star> stars, int k) {
+        return findClosestKStars(stars.iterator(), k);
     }
-  }
 
-  public static List<Star> findClosestKStars(Iterator<Star> stars, int k) {
-    // TODO - you fill in here.
-    return Collections.emptyList();
-  }
-  @EpiTest(testDataFile = "k_closest_stars.tsv")
-  public static List<Star> findClosestKStarsWrapper(List<Star> stars, int k) {
-    return findClosestKStars(stars.iterator(), k);
-  }
+    @EpiTestExpectedType
+    public static List<Double> expectedType;
 
-  @EpiTestExpectedType public static List<Double> expectedType;
-
-  @EpiTestComparator
-  public static boolean comp(List<Double> expected, List<Star> result) {
-    if (expected.size() != result.size()) {
-      return false;
+    @EpiTestComparator
+    public static boolean comp(List<Double> expected, List<Star> result) {
+        if (expected.size() != result.size()) {
+            return false;
+        }
+        Collections.sort(result);
+        for (int i = 0; i < result.size(); i++) {
+            if (result.get(i).distance() != expected.get(i)) {
+                return false;
+            }
+        }
+        return true;
     }
-    Collections.sort(result);
-    for (int i = 0; i < result.size(); i++) {
-      if (result.get(i).distance() != expected.get(i)) {
-        return false;
-      }
-    }
-    return true;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "KClosestStars.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "KClosestStars.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
