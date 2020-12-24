43d4d125608b174353f50a518b9c581e2c06563a
rwise <robertwise64@gmail.com>
2020-12-24 15:19:29 -0600

    Calendar Rendering

--- epi_judge_java/epi/CalendarRendering.java
+++ epi_judge_java/epi/CalendarRendering.java
@@ -3,6 +3,8 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 public class CalendarRendering {
   @EpiUserType(ctorParams = {int.class, int.class})
@@ -28,9 +30,40 @@ public class CalendarRendering {
 
   @EpiTest(testDataFile = "calendar_rendering.tsv")
 
+  /*
+  Just compile all the intervals into a list and sort them
+  Time: O(nlogn), where n is the number of events
+  Space: O(n)
+   */
   public static int findMaxSimultaneousEvents(List<Event> A) {
-    // TODO - you fill in here.
-    return 0;
+    List<Endpoint> points = new ArrayList<>();
+    // Compile times into a single list
+    for (Event e : A) {
+        points.add(new Endpoint(e.start, true));
+        points.add(new Endpoint(e.finish, false));
+    }
+    // Sort the points
+    Collections.sort(points, (p1, p2) -> {
+        int comp = Integer.compare(p1.time, p2.time);
+        // Default to regular comparison if not equal
+        if (comp != 0)
+            return comp;
+        // If same, want starting intervals to occur first
+        else
+            return (p1.isStart) ? -1 : 1;
+    });
+    int maxOverlap = 0;
+    int curEvents = 0;
+    // Count the overlaps
+    for (Endpoint p : points) {
+        if (p.isStart) {
+            curEvents++;
+            maxOverlap = Math.max(maxOverlap, curEvents);
+        }
+        else
+            curEvents--;
+    }
+    return maxOverlap;
   }
 
   public static void main(String[] args) {
