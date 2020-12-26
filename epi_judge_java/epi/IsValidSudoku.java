57ff74dca8c98f4599d29e70575219f72343bc4f
rwise <robertwise64@gmail.com>
2020-12-26 13:33:03 -0600

    Is Valid Sudoku

--- epi_judge_java/epi/IsValidSudoku.java
+++ epi_judge_java/epi/IsValidSudoku.java
@@ -7,8 +7,27 @@ public class IsValidSudoku {
   @EpiTest(testDataFile = "is_valid_sudoku.tsv")
 
   // Check if a partially filled matrix has any conflicts.
+  /*
+  Solves using a length 9 array for each row, column, and square. Could be done without...
+  Time: O(1), since it'll always iterate through the n x n partialAssignment once, but n always = 9
+  Space: Also O(1) for the same reason, but we could again reduce it
+   */
   public static boolean isValidSudoku(List<List<Integer>> partialAssignment) {
-    // TODO - you fill in here.
+    int[][] rows = new int[9][9];
+    int[][] cols = new int[9][9];
+    int[][] squares = new int[9][9];
+    for (int i = 0; i < partialAssignment.size(); i++) {
+      for (int j = 0; j < partialAssignment.get(0).size(); j++) {
+        int cur = partialAssignment.get(i).get(j);
+        if (cur != 0) {
+          if (rows[i][cur - 1] != 0 || cols[j][cur - 1] != 0 || squares[((j / 3) * 3) + i / 3][cur - 1] != 0)
+            return false;
+          rows[i][cur - 1] = 1;
+          cols[j][cur - 1] = 1;
+          squares[((j / 3) * 3) + i / 3][cur - 1] = 1;
+        }
+      }
+    }
     return true;
   }
 
