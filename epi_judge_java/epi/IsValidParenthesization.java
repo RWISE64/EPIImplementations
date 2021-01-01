99f3225b3eb8fa2b399a4314cef91ba58a9b4775
rwise <robertwise64@gmail.com>
2021-01-01 17:32:38 -0600

    Is Valid Parenthesization

--- epi_judge_java/epi/IsValidParenthesization.java
+++ epi_judge_java/epi/IsValidParenthesization.java
@@ -1,19 +1,50 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class IsValidParenthesization {
-  @EpiTest(testDataFile = "is_valid_parenthesization.tsv")
+    @EpiTest(testDataFile = "is_valid_parenthesization.tsv")
 
-  public static boolean isWellFormed(String s) {
-    // TODO - you fill in here.
-    return true;
-  }
+    /*
+    Just use a stack to track the most recently open parenthesis
+    Tried to use some extra data structures to cut down on the if statements
+    Time: O(n), where n = s.length()
+    Space: O(n) - stores up to half of s
+     */
+    public static boolean isWellFormed(String s) {
+        ArrayDeque<Character> openParenthesisStack = new ArrayDeque<>();
+        HashSet<Character> openParenthesis = new HashSet<>();
+        openParenthesis.addAll(Arrays.asList('{', '[', '('));
+        HashMap<Character, Character> closedParenthesisToOpen = new HashMap<>();
+        closedParenthesisToOpen.put('}', '{');
+        closedParenthesisToOpen.put(']', '[');
+        closedParenthesisToOpen.put(')', '(');
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if (openParenthesis.contains(cur)) {
+                openParenthesisStack.push(cur);
+            }
+            // Invalid if there are no open parenthesis or the current closing parenthesis doesn't equal the most recently opened
+            else if (openParenthesisStack.isEmpty() || closedParenthesisToOpen.get(cur) != openParenthesisStack.pop()) {
+                return false;
+            }
+        }
+        // False if there are some remaining open parenthesis
+        return openParenthesisStack.isEmpty();
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsValidParenthesization.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IsValidParenthesization.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
